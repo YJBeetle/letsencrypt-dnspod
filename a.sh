f563c009f595a7c308a8552187eb2e6525be834f
YJBeetle <YJBeetle@gmail.com>
2017-01-22 19:10:44 +0800

    创建记录 完毕

--- a.sh
+++ a.sh
@@ -99,12 +99,60 @@ get_record_id()
     done
 }
 
+create_record()
+{
+    login_token=$1
+    record=$2
+    domain_id=$3
+
+    local DOMLVL=0 #初始化节点
+
+    curl -k https://dnsapi.cn/Record.Create -d "login_token=$login_token&domain_id=$domain_id&sub_domain=$record&record_type=TXT&record_line=默认&value=null" 2>/dev/null >./tmp/tmp.xml
+    while read_xml_dom; do
+        if [ "$ENTITY" = 'id' ]; then
+            id="$CONTENT"
+        fi
+        if [ "$ENTITY" = 'code' ]; then
+            code=$CONTENT
+        fi
+        if [ "$ENTITY" = 'message' ]; then
+            message="$CONTENT"
+        fi
+    done < ./tmp/tmp.xml
+
+    if [ "$code" = '1' ]; then
+        echo "$id";
+        return 0;
+    else
+        echo "$message";
+        return $code;
+    fi
+
+}
+
+echo -n '初始化...'
+mkdir -p ./tmp/
+echo 'done'
+
+echo -n '读取配置文件...'
 . ./config.sh
+echo 'done'
 
+echo -n '获取domain_id...'
 domain_id=$(get_domain_id $login_token $domain)
-record_id=$(get_record_id $login_token $record $domain_id)
-if [ "$record_id" = '' ]; then
-    echo '没有找到记录'
-fi
+echo "$domain_id"
 
+echo -n '获取record_id...'
+record_id=$(get_record_id $login_token $record $domain_id)
 echo "$record_id"
+
+if [ "$record_id" = '' ]; then
+    echo -n '没有找到对应record，创建record并获取id...'
+    record_id=$(create_record "$login_token" "$record" "$domain_id") || 
+    {
+        echo "[error]"
+        echo "错误消息：$record_id" 1>&2 
+        exit 1
+    }
+    echo "$record_id"
+fi
