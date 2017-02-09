f47c3b6bc599e74e71bda0a11e00f600ccfd0ad1
YJBeetle <YJBeetle@gmail.com>
2017-02-09 17:24:27 +0800

    改用letsencrypt的临时环境url

--- letsencrypt-dnspod.sh
+++ letsencrypt-dnspod.sh
@@ -283,6 +283,7 @@ load_config() {
 
   # Default values
   CA="https://acme-v01.api.letsencrypt.org/directory"
+  CA="https://acme-staging.api.letsencrypt.org/directory"
   LICENSE="https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf"
   CERTDIR=
   ACCOUNTDIR=
