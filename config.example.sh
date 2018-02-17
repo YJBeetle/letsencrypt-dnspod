fd495b963cac771d09e7b961acaea871c4ea59b7
俞亦键 <yjbeetle@gmail.com>
2018-02-18 02:22:00 +0800

    update LE

--- config.example.sh
+++ config.example.sh
@@ -15,4 +15,4 @@ CONTACT_EMAIL=  #联系人邮箱（可选，如果填写则将会在注册时提
 RENEW_DAYS="30" #证书有效期，超过这个日期，执行时将会更新证书
 KEY_ALGO=rsa    #可选类型：rsa|prime256v1|secp384r1
 KEYSIZE="4096"  #RSA密钥长度（只有KEY_ALGO=rsa时候有效）
-LICENSE="https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf"
+LICENSE="https://letsencrypt.org/documents/LE-SA-v1.2-November-15-2017.pdf"
